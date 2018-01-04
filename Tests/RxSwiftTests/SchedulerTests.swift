53781378d0a03da0d4da9ddc53d43fe46cabfb58
beeth0ven <beeth0vendev@163.com>
2018-01-04 11:12:51 +0800

    Updates Scheduler tests

--- Tests/RxSwiftTests/SchedulerTests.swift
+++ Tests/RxSwiftTests/SchedulerTests.swift
@@ -29,99 +29,150 @@ final class SerialDispatchQueueSchedulerTests: RxTest {
 
 extension ConcurrentDispatchQueueSchedulerTests {
     func test_scheduleRelative() {
-        let expectScheduling = expectation(description: "wait")
-        let start = Date()
-
-        var interval = 0.0
-
-        let scheduler = self.createScheduler()
-
-        _ = scheduler.scheduleRelative(1, dueTime: 0.5) { (_) -> Disposable in
-            interval = Date().timeIntervalSince(start)
-            expectScheduling.fulfill()
-            return Disposables.create()
+        
+        typealias ScheduleAction = (Int) -> Disposable
+        
+        let scheduleRelatives: [(SchedulerType, @escaping ScheduleAction) -> Disposable] = [
+        { scheduler, action in scheduler.scheduleRelative(1, dueTime: 0.5, action: action) },
+        { scheduler, action in scheduler.scheduleRelative(1, dueTime: .milliseconds(500), action: action) },
+        { scheduler, action in scheduler.scheduleRelative(1, dueTime: .microseconds(500_000), action: action) },
+        ]
+        
+        for scheduleRelative in scheduleRelatives {
+            
+            let expectScheduling = expectation(description: "wait")
+            let start = Date()
+            
+            var interval = 0.0
+            
+            let scheduler = self.createScheduler()
+            
+            _ = scheduleRelative(scheduler) { (_) -> Disposable in
+                interval = Date().timeIntervalSince(start)
+                expectScheduling.fulfill()
+                return Disposables.create()
+            }
+            
+            waitForExpectations(timeout: 1.0) { error in
+                XCTAssertNil(error)
+            }
+            
+            XCTAssertEqual(interval, 0.5, accuracy: 0.2)
+            
         }
-
-        waitForExpectations(timeout: 1.0) { error in
-            XCTAssertNil(error)
-        }
-
-        XCTAssertEqual(interval, 0.5, accuracy: 0.2)
     }
 
     func test_scheduleRelativeCancel() {
-        let expectScheduling = expectation(description: "wait")
-        let start = Date()
-
-        var interval = 0.0
-
-        let scheduler = self.createScheduler()
-
-        let disposable = scheduler.scheduleRelative(1, dueTime: 0.1) { (_) -> Disposable in
-            interval = Date().timeIntervalSince(start)
-            expectScheduling.fulfill()
-            return Disposables.create()
+        
+        typealias ScheduleAction = (Int) -> Disposable
+        
+        let scheduleRelatives: [(SchedulerType, @escaping ScheduleAction) -> Disposable] = [
+        { scheduler, action in scheduler.scheduleRelative(1, dueTime: 0.1, action: action) },
+        { scheduler, action in scheduler.scheduleRelative(1, dueTime: .milliseconds(100), action: action) },
+        { scheduler, action in scheduler.scheduleRelative(1, dueTime: .microseconds(100_000), action: action) },
+        ]
+        
+        for scheduleRelative in scheduleRelatives {
+            
+            let expectScheduling = expectation(description: "wait")
+            let start = Date()
+            
+            var interval = 0.0
+            
+            let scheduler = self.createScheduler()
+            
+            let disposable = scheduleRelative(scheduler) { (_) -> Disposable in
+                interval = Date().timeIntervalSince(start)
+                expectScheduling.fulfill()
+                return Disposables.create()
+            }
+            disposable.dispose()
+            
+            DispatchQueue.main.asyncAfter (deadline: .now() + .milliseconds(200)) {
+                expectScheduling.fulfill()
+            }
+            
+            waitForExpectations(timeout: 0.5) { error in
+                XCTAssertNil(error)
+            }
+            
+            XCTAssertEqual(interval, 0.0, accuracy: 0.0)
+            
         }
-        disposable.dispose()
-
-        DispatchQueue.main.asyncAfter (deadline: .now() + .milliseconds(200)) {
-            expectScheduling.fulfill()
-        }
-
-        waitForExpectations(timeout: 0.5) { error in
-            XCTAssertNil(error)
-        }
-
-        XCTAssertEqual(interval, 0.0, accuracy: 0.0)
     }
 
     func test_schedulePeriodic() {
-        let expectScheduling = expectation(description: "wait")
-        let start = Date()
-        var times = [Date]()
-
-        let scheduler = self.createScheduler()
-
-        let disposable = scheduler.schedulePeriodic(0, startAfter: 0.2, period: 0.3) { (state) -> Int in
-            times.append(Date())
-            if state == 1 {
-                expectScheduling.fulfill()
+        
+        typealias ScheduleAction = (Int) -> Int
+        
+        let schedulePeriodics: [(SchedulerType, @escaping ScheduleAction) -> Disposable] = [
+        { scheduler, action in scheduler.schedulePeriodic(0, startAfter: 0.2, period: 0.3, action: action) },
+        { scheduler, action in scheduler.schedulePeriodic(0, startAfter: .milliseconds(200), period: .milliseconds(300), action: action) },
+        { scheduler, action in scheduler.schedulePeriodic(0, startAfter: .microseconds(200_000), period: .microseconds(300_000), action: action) },
+        ]
+        
+        for schedulePeriodic in schedulePeriodics {
+            
+            let expectScheduling = expectation(description: "wait")
+            let start = Date()
+            var times = [Date]()
+            
+            let scheduler = self.createScheduler()
+            
+            let disposable = schedulePeriodic(scheduler) { (state) -> Int in
+                times.append(Date())
+                if state == 1 {
+                    expectScheduling.fulfill()
+                }
+                return state + 1
             }
-            return state + 1
+            
+            waitForExpectations(timeout: 1.0) { error in
+                XCTAssertNil(error)
+            }
+            
+            disposable.dispose()
+            
+            XCTAssertEqual(times.count, 2)
+            XCTAssertEqual(times[0].timeIntervalSince(start), 0.2, accuracy: 0.1)
+            XCTAssertEqual(times[1].timeIntervalSince(start), 0.5, accuracy: 0.2)
+            
         }
-
-        waitForExpectations(timeout: 1.0) { error in
-            XCTAssertNil(error)
-        }
-
-        disposable.dispose()
-
-        XCTAssertEqual(times.count, 2)
-        XCTAssertEqual(times[0].timeIntervalSince(start), 0.2, accuracy: 0.1)
-        XCTAssertEqual(times[1].timeIntervalSince(start), 0.5, accuracy: 0.2)
     }
 
     func test_schedulePeriodicCancel() {
-        let expectScheduling = expectation(description: "wait")
-        var times = [Date]()
-
-        let scheduler = self.createScheduler()
-
-        let disposable = scheduler.schedulePeriodic(0, startAfter: 0.2, period: 0.3) { (state) -> Int in
-            times.append(Date())
-            return state + 1
+        
+        typealias ScheduleAction = (Int) -> Int
+        
+        let schedulePeriodics: [(SchedulerType, @escaping ScheduleAction) -> Disposable] = [
+        { scheduler, action in scheduler.schedulePeriodic(0, startAfter: 0.2, period: 0.3, action: action) },
+        { scheduler, action in scheduler.schedulePeriodic(0, startAfter: .milliseconds(200), period: .milliseconds(300), action: action) },
+        { scheduler, action in scheduler.schedulePeriodic(0, startAfter: .microseconds(200_000), period: .microseconds(300_000), action: action) },
+        ]
+        
+        for schedulePeriodic in schedulePeriodics {
+            
+            let expectScheduling = expectation(description: "wait")
+            var times = [Date]()
+            
+            let scheduler = self.createScheduler()
+            
+            let disposable = schedulePeriodic(scheduler) { (state) -> Int in
+                times.append(Date())
+                return state + 1
+            }
+            
+            disposable.dispose()
+            
+            DispatchQueue.main.asyncAfter (deadline: .now() + .milliseconds(300)) {
+                expectScheduling.fulfill()
+            }
+            
+            waitForExpectations(timeout: 1.0) { error in
+                XCTAssertNil(error)
+            }
+            
+            XCTAssertEqual(times.count, 0)
         }
-
-        disposable.dispose()
-
-        DispatchQueue.main.asyncAfter (deadline: .now() + .milliseconds(300)) {
-            expectScheduling.fulfill()
-        }
-
-        waitForExpectations(timeout: 1.0) { error in
-            XCTAssertNil(error)
-        }
-
-        XCTAssertEqual(times.count, 0)
     }
 }
